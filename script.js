b337610c73a4b3d3f38b1ad8d585ed8a2a264f46
m0nSSS <116755976+m0nSSS@users.noreply.github.com>
2022-12-06 23:04:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let body = document.querySelector("body");
+let button = document.querySelector("button");
+let JokeBlock = document.createElement("p");
+document.body.appendChild(JokeBlock);
+JokeBlock.classList.add("joke");
+let EmojiBlock = document.createElement("p");
+document.body.appendChild(EmojiBlock);
+EmojiBlock.style.display = "none";
+
+const emojis = ['😀','😃','😄','😁','😆','😅','🤣','😂','🙂','🙃']
+
+function ErrJoke() {
+    JokeBlock.style.display = "block";
+    EmojiBlock.textContent = emojis[ Math.floor(Math.random() * emojis.length)];
+    EmojiBlock.style.display = "block";
+    JokeBlock.textContent = "Ууууупс! Что-то пошло не так!";
+}
+
+button.addEventListener("click", () => {
+    fetch("https://api.chucknorris.io/jokes/random/")
+    .then (res => res.json())
+    .then ((data) => {
+    JokeBlock.textContent = data.value;
+    JokeBlock.style.display = "block";
+     EmojiBlock.textContent = emojis[ Math.floor(Math.random() * emojis.length)];
+    EmojiBlock.style.display = "block";
+    return JokeBlock.textContent;})
+    .catch((err) => ErrJoke())
+}
+)
